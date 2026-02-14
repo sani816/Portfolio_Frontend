5f2be344c55d8b6a47052bbe41c9e560d9b3109f
sani816 <saniakhatun622@gmail.com>
2026-02-15 01:36:14 +0530

    first commit

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const message = formData.get("message");
+
+    try {
+        const res = await fetch("http://127.0.0.1:5000/contact", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, email, message })
+        });
+
+        const data = await res.json();
+        console.log("Response from backend:", data); // check in console/Postman
+
+        if (data.success) {
+            alert("Message sent successfully!");
+            form.reset();
+        } else {
+            alert(data.error || "Error sending message");
+        }
+
+    } catch (err) {
+        alert("Server not connected!");
+        console.log(err);
+    }
+});
