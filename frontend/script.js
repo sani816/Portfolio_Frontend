de681dc81cda45c434bacd89c5e5eca353627c23
Sania Khatun <saniakhatun622@gmail.com>
2026-02-15 17:06:07 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,7 @@ form.addEventListener("submit", async (e) => {
     const message = formData.get("message");
 
     try {
-        const res = await fetch("http://127.0.0.1:5000/contact", {
+        const res = await fetch("/api/contact", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, email, message })
